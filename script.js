a6b5943d5e43bba108531cfb6b6bf5697676b13c
sumit <sumit7288.ca23@chitkara.edu.in>
2025-03-21 00:15:00 +0530

    done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const passwordbox = Document.getElementById("password");
+const passwordbox = document.getElementById("password");
 const length = 12;
 
 const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -6,10 +6,22 @@ const lowercase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
 const symbol = "!@#$%^&*()_-{}[]:'',>.<";
 
+const allchars = uppercase + lowercase + number + symbol;
+
 function createpassword(){
     let password = "";
     password += uppercase[Math.floor(Math.random() * uppercase.length)];
     password += lowercase[Math.floor(Math.random() * lowercase.length)];
     password += number[Math.floor(Math.random() * number.length)];
     password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while (password.length < length) {
+        password += allchars[Math.floor(Math.random() * allchars.length)];
+    }
+    
+    passwordbox.value = password;
+}
+function copypassword(){
+    passwordbox.select();
+    document.execCommand("copy");
 }
